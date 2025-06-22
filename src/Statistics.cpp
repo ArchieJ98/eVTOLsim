c976778e252f0407202590c3e584357305e35631
ArchieJ98 <archietvm@gmail.com>
2025-06-22 14:41:45 -0700

    Statistics.cpp

---
+++ src/Statistics.cpp
@@ -0,0 +1,49 @@
+#include "Statistics.h"
+using namespace std;
+
+#include <iostream>
+#include <iomanip>
+
+void Statistics::logFlight(const string &vehicle_type, double flight_time, double distance, int passengers)
+{
+    Stats &s = StatMap[vehicle_type];
+    s.total_FlightTime += flight_time;
+    s.total_Distance += distance;
+    s.total_PassengerMiles += static_cast<int>(distance * passengers);
+    s.flight_count++; 
+}
+
+void Statistics::logCharge(const string &vehicle_type, double charge_time)
+{
+    Stats &s = StatMap[vehicle_type];
+    s.total_ChargingTime += charge_time;
+
+    //Assuming multiple charges scenario
+    s.charge_count++;
+}
+
+void Statistics::logFaults(const string &vehicle_type)
+{
+    StatMap[vehicle_type].total_Faults++;
+}
+
+void Statistics::printStats()
+{
+    cout << "\n--- Simulation Summary ---\n";
+
+    for(const auto &vehicle: StatMap)
+    {
+        const string &type = vehicle.first;
+        const Stats &s = vehicle.second;
+
+        cout << "\nVehicle Type: " << type << endl;
+        cout << "Total FLight Time =  " << s.total_FlightTime << endl;
+        cout << "Total Distance Flown = " << s.total_Distance << endl;
+        cout << "Total Charging time = " << s.total_ChargingTime << endl;
+        cout << "Total Passenger Miles = " << s.total_PassengerMiles << endl;
+        cout << "Total Faults = " << s.total_Faults << endl;
+        cout << "Number of Flights = " << s.flight_count << endl;
+        cout << "Number of times charged = " << s.charge_count << endl;
+
+    }
+}
