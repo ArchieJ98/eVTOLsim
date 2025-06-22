99b899db099d49c891bb07377fe5e4efad3e7e6e
ArchieJ98 <archietvm@gmail.com>
2025-06-22 15:07:31 -0700

    Simulation.h

---
+++ headers/Simulation.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "Vehicle.h"
+#include "Vehicle_Instance.h"
+#include "Statistics.h"
+#include "ChargerManager.h"
+
+class Simulation{
+    private:
+        std::vector<Vehicle> vehicle_Types;
+        std::vector<VehicleInstance> vehicles;
+        ChargerManager chargers;
+        Statistics stats;
+
+        double sim_time;        //Total time taken to simulate
+        double current_time;    //Keeps track of current time during the simulation
+        double time_Increment;  //Value by witch time increases in the simulation
+
+    public:
+        Simulation();
+        void vehicle_Init();   //Load vehicle definitions and create instances
+        void run_Sim();        // Begins the Simulation
+};
