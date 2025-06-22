5536a78206c95aed4781de4c8bad96b7ece7debf
ArchieJ98 <archietvm@gmail.com>
2025-06-22 12:51:11 -0700

    ChargerManager.cpp

---
+++ src/ChargerManager.cpp
@@ -0,0 +1,43 @@
+#include "ChargerManager.h"
+using namespace std;
+
+ChargerManager::ChargerManager(int availableChargers): maxChargers(availableChargers){}
+
+bool ChargerManager::charger_Availability() const
+{
+    return charging_Vehicles.size() < maxChargers;
+}
+
+void ChargerManager::charge_Request(VehicleInstance *v)
+{
+    if(charger_Availability())
+    {
+        charging_Vehicles.push_back(v);
+    }
+    else{
+        waiting_Queue.push(v);
+    }
+}
+
+void ChargerManager::charge_Update(double time_increment)
+{
+    vector<VehicleInstance*> stillCharging;
+
+    for(VehicleInstance *v: charging_Vehicles)
+    {
+        v->charge(time_increment);
+        if(!v->charge_completed())
+        {
+            stillCharging.push_back(v);
+        }
+    }
+
+    charging_Vehicles = stillCharging;
+
+    while(!waiting_Queue.empty() && charger_Availability())
+    {
+        VehicleInstance *next = waiting_Queue.front();
+        waiting_Queue.pop();
+        charging_Vehicles.push_back(next);
+    }
+}
